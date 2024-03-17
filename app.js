de17a96c22a0562e737522b1114ad953b0a2dc97
Badis Jlassi <98410957+badisjl99@users.noreply.github.com>
2024-03-17 01:13:10 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const dotenv = require('dotenv');
 const TelegramBot = require('node-telegram-bot-api');
 const { MongoClient } = require('mongodb');
+const pm2 = require('pm2');
+
 dotenv.config();
 
 const token = process.env.BOT_API_TOKEN;
@@ -18,47 +20,50 @@ async function connectToMongoDB() {
     }
 }
 
-const bot = new TelegramBot(token, { polling: true });
-bot.deleteWebHook();
-console.log('Bot is ru1nning...');
-
-bot.onText(/\/start/, (msg) => {
-    const chatId = msg.chat.id;
-    const username = msg.from.username;
-    bot.sendMessage(chatId, `Hello ${username}, How may I help you today?`)
-        .catch(error => console.error('Error:', error));
-});
-
-bot.onText(/\/about/, (msg) => {
-    const chatId = msg.chat.id;
-    const message = `🎬 Welcome to MovieMagnet! 🤖\n\nDiscover the latest blockbusters effortlessly. 🌟\nInstantly download your favorite movies in HD. 🎥\nStay ahead with our curated selection of the newest releases. 🍿\nExperience cinema at your fingertips with MovieMagnet! 🌟🤖`;
-    bot.sendMessage(chatId, message);
-});
-
-bot.onText(/\/randommovie/, async (msg) => {
-    const chatId = msg.chat.id;
-
+async function startBot() {
     try {
         const moviesCollection = await connectToMongoDB();
         
-        const randomMovies = await moviesCollection.aggregate([
-            { $match: { rating: { $gt: "7" }, year: { $gte: "2000" } } },
-            { $sample: { size: 1 } }
-        ]).toArray();
+        const bot = new TelegramBot(token, { polling: { interval: 1000 } });
+        bot.deleteWebHook();
+        console.log('Bot is running...');
 
-        randomMovies.forEach(async movie => {
-            let summary = movie.summary;
-            if (summary.length > 270) {
-                summary = summary.substring(0, 270) + '...';
-            }
+        bot.onText(/\/start/, (msg) => {
+            const chatId = msg.chat.id;
+            const username = msg.from.username;
+            bot.sendMessage(chatId, `Hello ${username}, How may I help you today?`)
+                .catch(error => console.error('Error:', error));
+        });
 
-            const genres = movie.genres.join(', '); // Convert array to comma-separated string
-            const actors = movie.actors.map(actor => `${actor.name} as ${actor.role}`).join('\n');
+        bot.onText(/\/about/, (msg) => {
+            const chatId = msg.chat.id;
+            const message = `🎬 Welcome to MovieMagnet! 🤖\n\nDiscover the latest blockbusters effortlessly. 🌟\nInstantly download your favorite movies in HD. 🎥\nStay ahead with our curated selection of the newest releases. 🍿\nExperience cinema at your fingertips with MovieMagnet! 🌟🤖`;
+            bot.sendMessage(chatId, message);
+        });
 
-            // Format the trailer link with emoji and clickable URL
-            const trailerLink = `[🎬 Watch Trailer](${movie.trailerLink})`;
+        bot.onText(/\/randommovie/, async (msg) => {
+            const chatId = msg.chat.id;
 
-            const message = `
+            try {
+                const moviesCollection = await connectToMongoDB();
+                
+                const randomMovies = await moviesCollection.aggregate([
+                    { $match: { rating: { $gt: "7" }, year: { $gte: "2000" } } },
+                    { $sample: { size: 1 } }
+                ]).toArray();
+
+                randomMovies.forEach(async movie => {
+                    let summary = movie.summary;
+                    if (summary.length > 270) {
+                        summary = summary.substring(0, 270) + '...';
+                    }
+
+                    const genres = movie.genres.join(', ');
+                    const actors = movie.actors.map(actor => `${actor.name} as ${actor.role}`).join('\n');
+
+                    const trailerLink = `[🎬 Watch Trailer](${movie.trailerLink})`;
+
+                    const message = `
 🎬 *Title:*      *${movie.title}*\n
 ⭐️ *Rating:* ${movie.rating}
 📅 *Year:* ${movie.year}
@@ -69,31 +74,32 @@ ${actors}\n
  [${trailerLink}]
 `;
 
-            const downloadButtons = movie.download.map(download => ({
-                text: `${download.quality}`,
-                url: download.link
-            }));
+                    const downloadButtons = movie.download.map(download => ({
+                        text: `${download.quality}`,
+                        url: download.link
+                    }));
 
-            const inline_keyboard = downloadButtons.map(button => ([{ text: button.text, url: button.url }]));
+                    const inline_keyboard = downloadButtons.map(button => ([{ text: button.text, url: button.url }]));
 
-            await bot.sendPhoto(chatId, movie.imageUrl, {
-                caption: message,
-                parse_mode: 'Markdown',
-                reply_markup: {
-                    inline_keyboard: inline_keyboard
-                }
-            });
+                    await bot.sendPhoto(chatId, movie.imageUrl, {
+                        caption: message,
+                        parse_mode: 'Markdown',
+                        reply_markup: {
+                            inline_keyboard: inline_keyboard
+                        }
+                    });
+                });
+
+                await moviesCollection.client.close();
+            } catch (error) {
+                console.error('Error:', error);
+                bot.sendMessage(chatId, 'An error occurred while processing your request');
+            }
         });
 
-        await moviesCollection.client.close();
-    } catch (error) {
-        console.error('Error:', error);
-        bot.sendMessage(chatId, 'An error occurred while processing your request');
-    }
-});
-bot.onText(/\/help/, (msg) => {
-    const chatId = msg.chat.id;
-    const message = `
+        bot.onText(/\/help/, (msg) => {
+            const chatId = msg.chat.id;
+            const message = `
 🤖 *MovieMagnet Bot Help* 🤖\n\n
 Use the following *commands* to interact with the bot:\n
 /help - Display available commands and their descriptions.
@@ -102,54 +108,54 @@ Use the following *commands* to interact with the bot:\n
 /displaygenres - Display all available genres.
 /genre (Movie genre Choice) - Display Random Movie With Specified Genre (example : /genre crime) 
 `;
-    bot.sendMessage(chatId, message, { parse_mode: 'Markdown' });
-});
+            bot.sendMessage(chatId, message, { parse_mode: 'Markdown' });
+        });
 
-bot.onText(/\/displaygenres/, async (msg) => {
-    const chatId = msg.chat.id;
+        bot.onText(/\/displaygenres/, async (msg) => {
+            const chatId = msg.chat.id;
 
-    try {
-        const moviesCollection = await connectToMongoDB();
-        const genres = await moviesCollection.distinct('genres');
+            try {
+                const moviesCollection = await connectToMongoDB();
+                const genres = await moviesCollection.distinct('genres');
 
-        const message = `🎭 *Available Genres* 🎭\n\n${genres.join('\n')}`;
-        bot.sendMessage(chatId, message, { parse_mode: 'Markdown' });
+                const message = `🎭 *Available Genres* 🎭\n\n${genres.join('\n')}`;
+                bot.sendMessage(chatId, message, { parse_mode: 'Markdown' });
 
-        await moviesCollection.client.close();
-    } catch (error) {
-        console.error('Error:', error);
-        bot.sendMessage(chatId, 'An error occurred while processing your request');
-    }
-});
-bot.onText(/^\/genre (.+)$/, async (msg, match) => {
-    const chatId = msg.chat.id;
-    const genre = match[1].toLowerCase();
+                await moviesCollection.client.close();
+            } catch (error) {
+                console.error('Error:', error);
+                bot.sendMessage(chatId, 'An error occurred while processing your request');
+            }
+        });
 
-    try {
-        const moviesCollection = await connectToMongoDB();
-        
-        const randomMovie = await moviesCollection.aggregate([
-            { $match: { genres: { $regex: new RegExp(genre, 'i') }, rating: { $gt: "7" }, year: { $gte: "2000" } } },
-            { $sample: { size: 1 } }
-        ]).next();
+        bot.onText(/^\/genre (.+)$/, async (msg, match) => {
+            const chatId = msg.chat.id;
+            const genre = match[1].toLowerCase();
 
-        if (!randomMovie) {
-            bot.sendMessage(chatId, 'No movie found for the specified genre.');
-            return;
-        }
+            try {
+                const moviesCollection = await connectToMongoDB();
+                
+                const randomMovie = await moviesCollection.aggregate([
+                    { $match: { genres: { $regex: new RegExp(genre, 'i') }, rating: { $gt: "7" }, year: { $gte: "2000" } } },
+                    { $sample: { size: 1 } }
+                ]).next();
 
-        let summary = randomMovie.summary;
-        if (summary.length > 270) {
-            summary = summary.substring(0, 270) + '...';
-        }
+                if (!randomMovie) {
+                    bot.sendMessage(chatId, 'No movie found for the specified genre.');
+                    return;
+                }
 
-        const genres = randomMovie.genres.join(', '); // Convert array to comma-separated string
-        const actors = randomMovie.actors.map(actor => `${actor.name} as ${actor.role}`).join('\n');
+                let summary = randomMovie.summary;
+                if (summary.length > 270) {
+                    summary = summary.substring(0, 270) + '...';
+                }
 
-        // Format the trailer link with emoji and clickable URL
-        const trailerLink = `[🎬 Watch Trailer](${randomMovie.trailerLink})`;
+                const genres = randomMovie.genres.join(', ');
+                const actors = randomMovie.actors.map(actor => `${actor.name} as ${actor.role}`).join('\n');
 
-        const message = `
+                const trailerLink = `[🎬 Watch Trailer](${randomMovie.trailerLink})`;
+
+                const message = `
 🎬 *Title:*      *${randomMovie.title}*\n
 ⭐️ *Rating:* ${randomMovie.rating}
 📅 *Year:* ${randomMovie.year}
@@ -160,39 +166,55 @@ ${actors}\n
 [${trailerLink}]
 `;
 
-        const downloadButtons = randomMovie.download.map(download => ({
-            text: `${download.quality}`,
-            url: download.link
-        }));
+                const downloadButtons = randomMovie.download.map(download => ({
+                    text: `${download.quality}`,
+                    url: download.link
+                }));
 
-        const inline_keyboard = downloadButtons.map(button => ([{ text: button.text, url: button.url }]));
+                const inline_keyboard = downloadButtons.map(button => ([{ text: button.text, url: button.url }]));
 
-        await bot.sendPhoto(chatId, randomMovie.imageUrl, {
-            caption: message,
-            parse_mode: 'Markdown',
-            reply_markup: {
-                inline_keyboard: inline_keyboard
+                await bot.sendPhoto(chatId, randomMovie.imageUrl, {
+                    caption: message,
+                    parse_mode: 'Markdown',
+                    reply_markup: {
+                        inline_keyboard: inline_keyboard
+                    }
+                });
+
+                await moviesCollection.client.close();
+            } catch (error) {
+                console.error('Error:', error);
+                bot.sendMessage(chatId, 'An error occurred while processing your request');
             }
         });
 
-        await moviesCollection.client.close();
     } catch (error) {
-        console.error('Error:', error);
-        bot.sendMessage(chatId, 'An error occurred while processing your request');
+        console.error('Error starting bot:', error);
+        process.exit(1);
     }
-});
+}
 
+pm2.connect((err) => {
+    if (err) {
+        console.error('Error connecting to PM2:', err);
+        process.exit(1);
+    }
 
+    pm2.list((err, list) => {
+        if (err) {
+            console.error('Error retrieving PM2 process list:', err);
+            pm2.disconnect();
+            process.exit(1);
+        }
 
-
-
-
-
-
-bot.deleteWebHook()
-    .then(() => {
-        console.log('Webhook deleted successfully');
-    })
-    .catch((error) => {
-        console.error('Error deleting webhook:', error.message);
+        const botProcess = list.find((process) => process.name === 'movie_bot');
+        if (botProcess) {
+            console.log('Bot process is already running with PID:', botProcess.pid);
+            pm2.disconnect();
+            process.exit(0);
+        } else {
+            startBot();
+            pm2.disconnect();
+        }
     });
+});
